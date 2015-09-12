f9da212b8ddbf143bb09fad0f517d2ca25eac685
Tony Bowden <tony@tmtm.com>
2015-09-12 15:09:38 +0100

    preparation for finding memberships

--- scraper.rb
+++ scraper.rb
@@ -20,6 +20,29 @@ class String
   end
 end
 
+def overlap(mem, term)
+  mS = mem[:start_date].to_s.empty?  ? '0000-00-00' : mem[:start_date]
+  mE = mem[:end_date].to_s.empty?    ? '9999-99-99' : mem[:end_date]
+  tS = term[:start_date].to_s.empty? ? '0000-00-00' : term[:start_date]
+  tE = term[:end_date].to_s.empty?   ? '9999-99-99' : term[:end_date]
+
+  return unless mS < tE && mE > tS
+  (s, e) = [mS, mE, tS, tE].sort[1,2]
+  return { 
+    _data: [mem, term],
+    start_date: s == '0000-00-00' ? nil : s,
+    end_date:   e == '9999-99-99' ? nil : e,
+  }
+end
+
+def combine(h)
+  into_name, into_data, from_name, from_data = h.flatten
+  from_data.product(into_data).map { |a,b| overlap(a,b) }.compact.map { |h|
+    data = h.delete :_data
+    h.merge({ from_name => data.first[:id], into_name => data.last[:id] })
+  }.sort_by { |h| h[:start_date] }
+end
+
 pages = [
   '/personal/deputati/saeima12_depweb_public.nsf/deputies?OpenView&lang=EN&count=1000',
   '/personal/deputati/saeima12_depweb_public.nsf/deputiesByMandate?OpenView&restricttocategory=1&lang=EN&count=1000',
@@ -53,23 +76,6 @@ pages.each do |link|
     end
     nokomem = Nokogiri::HTML(mp_page.html)
 
-    mems = nokomem.xpath('.//div[@class="header2" and contains(.,"Membership in the Saeima")]/following::table[1]//tr[td]').map do |tr|
-      mtds = tr.css('td')
-      mem = { 
-        start_date: mtds[0].text.split('.').reverse.join('-'),
-        end_date: mtds[1].text.split('.').reverse.join('-'),
-        what: mtds[2].text,
-        role: mtds[3].text,
-      }
-    end
-    binding.pry if mems.count.zero?
-    raise "Erk! No memberships for #{mp_page.current_url}" if mems.count.zero?
-    terms, groups = mems.partition { |m| m[:what].downcase.include? 'member of the saeima' }
-    binding.pry if terms.count.zero?
-    raise "Erk! No terms for #{mp_page.current_url}" if terms.count.zero?
-    binding.pry if groups.count.zero?
-    raise "Erk! No groups for #{mp_page.current_url}" if groups.count.zero?
-
     email = nokomem.css('table.wholeForm a[href^="mailto:"]/@href').text.gsub('mailto:','') rescue ''
     person = { 
       id: URI(mp_page.current_url).path.split('/').last,
@@ -79,6 +85,20 @@ pages.each do |link|
       source: mp_page.current_url,
     }
     person[:photo] = URI.join(url, person[:photo]).to_s unless person[:photo].to_s.empty?
+
+    mems = nokomem.xpath('.//div[@class="header2" and contains(.,"Membership in the Saeima")]/following::table[1]//tr[td]').map do |tr|
+      mtds = tr.css('td')
+      mem = { 
+        start_date: mtds[0].text.split('.').reverse.join('-'),
+        end_date: mtds[1].text.split('.').reverse.join('-'),
+        what: mtds[2].text,
+        role: mtds[3].text,
+      }
+    end
+    terms, groups = mems.partition { |m| m[:what].downcase.include? 'member of the saeima' }
+    binding.pry if terms.count.zero? || groups.count.zero?
+
+    binding.pry
 
     data = person.merge(front)
     puts data
