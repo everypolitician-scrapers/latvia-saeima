69721b6b21899a03701adfa2c935c0ff26f5d6f8
Tony Bowden <tony@tmtm.com>
2015-09-12 15:07:52 +0100

    lots of tidying

--- scraper.rb
+++ scraper.rb
@@ -15,7 +15,7 @@ Capybara.default_driver = :poltergeist
 @BASE = 'http://titania.saeima.lv'
 
 class String
-  def trim
+  def tidy
     self.gsub(/[[:space:]]/,' ').strip
   end
 end
@@ -28,59 +28,60 @@ pages = [
 
 pages.each do |link|
   url = @BASE + link
+  puts url
   page = visit url
 
   noko = Nokogiri::HTML(page.html)
 
   noko.css('table#tableWithContent tr[class*="Row"]').each do |row|
     tds = row.css('td')
-    mp_link = row.attr('onclick').split("'")[1]
+    front = {
+      first_name: tds[1].text.tidy,
+      family_name: tds[2].text.tidy,
+      current_group: tds[3].text.tidy,
+      source: row.attr('onclick').split("'")[1],
+    }
 
     # puts "#{mp_link}".red
-    mp_page = visit mp_link
-
+    mp_page = visit front[:source]
     mem_table = ''
     if mp_page.has_xpath? ('//frame[@name="topFrame"]') 
       framesrc = find(:xpath, '//frame[@name="topFrame"]')['src']
-      frame_url = URI.join(mp_link, framesrc)
+      frame_url = URI.join(front[:source], framesrc)
       # warn "Frame: #{frame_url}".red
       mp_page = visit frame_url
     end
+    nokomem = Nokogiri::HTML(mp_page.html)
 
-    mem_table = find(:xpath, './/div[@class="header2" and contains(.,"Membership in the Saeima")]/following::table[1]')
-
-    memberships = mem_table.all(:xpath, './/td[text() = "Member"]')
-    if memberships.count.zero?
-      memberships = mem_table.all(:xpath, './/td[contains(.,"Chair")]')
-      raise "Erk! No memberships for #{mp_page.current_url}" if memberships.count.zero?
+    mems = nokomem.xpath('.//div[@class="header2" and contains(.,"Membership in the Saeima")]/following::table[1]//tr[td]').map do |tr|
+      mtds = tr.css('td')
+      mem = { 
+        start_date: mtds[0].text.split('.').reverse.join('-'),
+        end_date: mtds[1].text.split('.').reverse.join('-'),
+        what: mtds[2].text,
+        role: mtds[3].text,
+      }
     end
+    binding.pry if mems.count.zero?
+    raise "Erk! No memberships for #{mp_page.current_url}" if mems.count.zero?
+    terms, groups = mems.partition { |m| m[:what].downcase.include? 'member of the saeima' }
+    binding.pry if terms.count.zero?
+    raise "Erk! No terms for #{mp_page.current_url}" if terms.count.zero?
+    binding.pry if groups.count.zero?
+    raise "Erk! No groups for #{mp_page.current_url}" if groups.count.zero?
 
-    raise "Too many memberships for #{mp_page.current_url}" if memberships.count > 1
-
-    mtds = memberships.first.all(:xpath, '../td')
-    start_date = mtds[0].text
-    end_date = mtds[1].text
-    faction = mtds[2].text
-    email = find('table.wholeForm a[href^=mailto]')['href'].gsub('mailto:','') rescue ''
-    binding.pry
-
-    data = { 
+    email = nokomem.css('table.wholeForm a[href^="mailto:"]/@href').text.gsub('mailto:','') rescue ''
+    person = { 
       id: URI(mp_page.current_url).path.split('/').last,
-      name: find('div.header3#ViewBlockTitle').text,
-      first_name: tds[1].text.strip,
-      family_name: tds[2].text.strip,
-      faction: tds[3].text.strip,
-      photo: find('td#photoHolder img')['src'],
+      name: nokomem.css('div.header3#ViewBlockTitle').text.tidy,
+      photo: nokomem.css('td#photoHolder img/@src').text,
       email: email,
-      faction: faction,
-      start_date: start_date,
-      end_date: end_date,
-      term: 12,
       source: mp_page.current_url,
     }
-    data[:photo].prepend @BASE unless data[:photo].to_s.empty?
-    ScraperWiki.save_sqlite([:id, :term], data)
+    person[:photo] = URI.join(url, person[:photo]).to_s unless person[:photo].to_s.empty?
 
+    data = person.merge(front)
     puts data
+    # ScraperWiki.save_sqlite([:id, :term], data)
   end
 end
